0109789df0c21ef2496ad6f2026f9e8757208fad
rajeeviiit2108 <rajeevi.iiit@gmail.com>
2020-05-06 15:05:26 +0530

    Merge sort algorithm in java
    Change-Id: If227c18573fb755814758db8eb945d49cb2f719b
    Signed-off-by: rajeeviiit2108 <rajeevi.iiit@gmail.com>

--- datastructures/src/datastructure/brainstorm/sorting/MergeSort.java
+++ datastructures/src/datastructure/brainstorm/sorting/MergeSort.java
@@ -1,10 +1,68 @@
 package datastructure.brainstorm.sorting;
 
-public class MergeSort {
+import java.util.Arrays;
 
+public class MergeSort
+{
 	public static void main(String[] args) {
-		System.out.println("how are you??");
-
+		int arr[] = {12, 11, 13, 5, 6, 7};
+		new MergeSort().mergerSortAlgo(arr, 0, arr.length-1);
+		System.out.println(Arrays.toString(arr));
 	}
-
+	
+	private void mergerSortAlgo(int [] arr, int low, int high)
+	{
+		if(low<high)
+		{
+			int mid=low+(high-low)/2;
+			mergerSortAlgo(arr, 0, mid);
+			mergerSortAlgo(arr, mid+1, high);
+			merge(arr,low, mid, high);
+		}
+	}
+	private void merge(int[] arr, int low, int mid, int high)
+	{
+		int len1=mid-low+1;
+		int len2=high-mid; //high-mid-1+1
+		
+		int[] leftArray=new int[len1];
+		int[] rightArray=new int[len2];
+		
+		for(int i=0; i<len1; i++)
+		{
+			leftArray[i]=arr[i+low];
+		}
+		for(int i=0; i<len2; i++)
+		{
+			rightArray[i]=arr[i+mid+1];
+		}
+		int i=0, j=0, k=low;
+		
+		while(i<len1 &&  j<len2)
+		{
+			if(leftArray[i]<=rightArray[j])
+			{
+				arr[k]=leftArray[i];
+				i++;
+			}
+			else
+			{
+				arr[k]=rightArray[j];
+				j++;
+			}
+			k++;
+		}
+		while(i<len1)
+		{
+			arr[k]=leftArray[i];
+			i++;
+			k++;
+		}
+		while(j<len2)
+		{
+			arr[k]=rightArray[j];
+			j++;
+			k++;
+		}
+	}
 }
